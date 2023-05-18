df895b30818f49ee76195cefef7580718a5470ec
highdizzy <mail.babak.heidary@gmail.com>
2023-05-18 19:17:26 +0200

    done

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-// Iteration 1: Names and Input
+let hacker1 = 'John'
+let hacker2 = 'Mahmoud'
+let count1 = hacker1.length
+let count2 = hacker2.length
 
+if (hacker1.length > hacker2.length) {
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+  }
+else {
+  console.log(`The navigator has the longest name, it has ${hacker2.length} characters`);
+}
 
-// Iteration 2: Conditionals
+console.log(hacker1.toUpperCase());
 
+function reverseString(str) {
+  let splitString = str.split("");
+  let reverseArray = splitString.reverse("");
+  let joinArray = reverseArray.join("");
+      console.log(reverseArray);
 
-// Iteration 3: Loops
+    return joinArray;
+    console.log(joinArray);
+
+}
+
+reverseString("John");
+
